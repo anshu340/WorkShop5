e4c18eaf86f78d4a5ea65af9f549292e6830f674
anshu340 <anshukarki533@gmail.com>
2025-11-25 08:20:45 +0545

    Task 5 (2) completed

---
+++ WorkShop5/Laptop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop5
+{
+    internal class Laptop : ElectronicDevice
+    {
+        // Constructor
+        public Laptop(string brand, double price) : base(brand, price)
+        {
+        }
+
+        // Extra method specific to Laptop
+        public void TurnOnBattery()
+        {
+            Console.WriteLine("Laptop battery is now turned on.");
+        }
+
+        // Override the abstract method
+        public override void ShowInfo()
+        {
+            Console.WriteLine(" Laptop Details ");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Price: ${Price}");
+        }
+    }
+}
---
+++ WorkShop5/Smartphone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkShop5
+{
+    internal class Smartphone : ElectronicDevice
+    {
+        // Constructor
+        public Smartphone(string brand, double price) : base(brand, price)
+        {
+        }
+
+        // Extra method specific to Smartphone
+        public void EnableCamera()
+        {
+            Console.WriteLine("Smartphone camera is now enabled.");
+        }
+
+        // Override the abstract method
+        public override void ShowInfo()
+        {
+            Console.WriteLine("=== Smartphone Details ===");
+            Console.WriteLine($"Brand: {Brand}");
+            Console.WriteLine($"Price: ${Price}");
+        }
+    }
+}
